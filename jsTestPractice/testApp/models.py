55063ea6eb7a54c7cd45f9e2614308b53a647493
Pelane Sanon <p_sanon@aol.com>
2018-12-03 13:58:45 -0600

    push 4

--- jsTestPractice/testApp/models.py
+++ jsTestPractice/testApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class TestModel(models.Model):
+    first_name = models.CharField(max_length=50, null=True)
+    last_name = models.CharField(max_length=50, null=True)
+    date_Submitted = models.DateTimeField(default=datetime.now)
+    username = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True,)
+
+    def __str__(self):
+        return self.first_name
--- jsTestPractice/testApp/views.py
+++ jsTestPractice/testApp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import TestModel
+from .forms import TestForm
+from datetime import datetime
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def userindex(request):
+    form_list = TestModel.objects.filter(username=request.user)
+    # post = get_object_or_404(TransactionModel, pk=pk)
+    context = {'form_list': form_list, }
+    return render(request, 'ExpenseApp/index.html', context)
