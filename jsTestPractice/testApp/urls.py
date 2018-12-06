d0cf46f4c433cf009b540f88dbba96f8c6379027
Pelane Sanon <p_sanon@aol.com>
2018-12-06 09:34:59 -0600

    push 7

--- jsTestPractice/testApp/urls.py
+++ jsTestPractice/testApp/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path, include
-from django.contrib import admin
 
 from . import views
 
 urlpatterns = [
     path('', views.userindex, name='userindex'),
-    path('admin/', admin.site.urls),
+
     ]
--- jsTestPractice/testApp/views.py
+++ jsTestPractice/testApp/views.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
+@login_required
 def userindex(request):
     form_list = TestModel.objects.filter(username=request.user)
     context = {'form_list': form_list, }
